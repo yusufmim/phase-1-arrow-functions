32de8f34c22c318873aaeafbdab447c818998bce
yussuf <yusufmim2001@gmail.com>
2025-03-09 22:06:25 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,16 @@
+const greet = function(name) {
+    return `Hello, ${name}!`;
+  };
+
+  
+  const square = num => num * num;
+
+  const add = (a, b) => {
+    console.log(`Adding ${a} and ${b}`);
+    return a + b;
+  };
+  
+  const divide = function() {
+    return 2000 / 100;
+  };
+  
